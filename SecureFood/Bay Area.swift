a049ed2a31743387c2dbce5b7873e0cfa2c81ee9
Layla <layla.hisham.sami@gmail.com>
2025-07-29 13:13:48 -0700

    added counties

---
+++ SecureFood/Bay Area.swift
@@ -0,0 +1,21 @@
+//
+//  Bay Area.swift
+//  SecureFood
+//
+//  Created by Layla Hisham on 7/29/25.
+//
+
+import SwiftUI
+
+struct Bay_Area: View {
+    var body: some View {
+        Text("Bay Area")
+            .font(.system(size:35))
+            .fontWeight(.black)
+Text("The Bay Area constitutes the countries of Sonoma, Napa, Solano, Marin, Contra Costa,San Mateo, ALameda, Santa Cruz, Santa Clara, San Benito, and Monterey")
+    }
+}
+
+#Preview {
+    Bay_Area()
+}
