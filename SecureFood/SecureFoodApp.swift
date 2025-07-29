100d4d03aa6e85c90895ca516570980935bdb08c
Aarna <aarnaburji@gmail.com>
2025-07-29 10:09:50 -0700

    Initial Commit

---
+++ SecureFood/SecureFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureFoodApp.swift
+//  SecureFood
+//
+//  Created by Aarna Burji on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SecureFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
