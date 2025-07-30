88765c1449113c7de083b1b33c404511bdedae38
Layla <layla.hisham.sami@gmail.com>
2025-07-30 10:41:21 -0700

    fixed errors

--- SecureFood/ContentView.swift
+++ SecureFood/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-Navigation Stack {
-            VStack {
+        NavigationStack {
+                    VStack {
                 
                 Text("Welcome to Secure Food!")
                     .font(.system(size:30))
