f89518628de4294f73d6dff20faeae814defcdcc
Priyanka <prirath20@gmail.com>
2025-07-30 14:16:43 -0700

    Central Valley page

--- SecureFood/Central Valley.swift
+++ SecureFood/Central Valley.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 struct Central_Valley: View {
     var body: some View {
-        Text("Central Valley")
-            .font(.system(size:35))
-            .fontWeight(.black)
-Text("The Central Valley constitutes the counties of Inyo, Tulare, Lings, Fresno, Madera, Mono, Merced, Mariposa, Tuolumne, Stanislaus, San Joaquin, Calaveras, Alpine, Yolo, Sacramento, Sutter, Yuba, Placer, El Dorado, and Amador")
+        ScrollView{
+            VStack(alignment: .leading, spacing: 20) {
+            Text("Central Valley")
+                .font(.system(size:35))
+                .fontWeight(.black)
+                .padding(.top, 20)
+            Text("The Central Valley constitutes the counties of Inyo, Tulare, Lings, Fresno, Madera, Mono, Merced, Mariposa, Tuolumne, Stanislaus, San Joaquin, Calaveras, Alpine, Yolo, Sacramento, Sutter, Yuba, Placer, El Dorado, and Amador")
+                .font(.body)
+            Text("Food Resources")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top)
+                Group{
+                    Link("Central California Food Bank", destination: URL(string: "https://ccfoodbank.org/")!)
+                    Link("Feeding America", destination: URL(string: "https://www.feedingamerica.org/find-your-local-foodbank/central-california-food-bank")!)
+                    Link("Second Harvest of the Greater Valley", destination: URL(string: "https://localfoodbank.org/")!)
+                    Link("California Association of Food Banks", destination: URL(string: "https://www.cafoodbanks.org/our-members/")!)
+                    Link("FindHelp", destination: URL(string: "https://www.findhelp.org/food/food-pantry--fresno-ca")!)
+                }
+            }
+            .padding()
+        }
     }
 }
 
